6bb6c7fdfefcb34e189b9f5802a9893f95507320
Paptoos Inc <72940447+Paptoos@users.noreply.github.com>
2020-12-05 10:23:41 +0330

    Update css.js

--- src/css.js
+++ src/css.js
@@ -1,9 +1,4 @@
 const CSS3 = (selector) =>{
-    let css_s=document.getElementById(selector)
+    let css_s=document.querySelector(selector)
     return css_s.style
 }
-const CSS3_class=(selector)=> {
-    let css_s=document.getElementsByClassName(selector)
-    return css_s.style
-}
-CSS3("ed").background="blue"
